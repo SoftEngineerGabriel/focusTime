efd67c130366ba67228ad14d19aeedc13838c595
SoftEngineerGabriel <123767611+SoftEngineerGabriel@users.noreply.github.com>
2023-04-29 15:08:25 -0300

    Add files via upload

--- focusTimer/script.js
+++ focusTimer/script.js
@@ -1,61 +1,94 @@
-// variables
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
-const buttonSoundON = document.querySelector('.sound-on')
-const buttonSoundMute = document.querySelector('.sound-mute')
-let minutes
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
 
-//functions
-function secondsCountdown(){
-    setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent)
-        if( seconds <= 0){
-            seconds = 60
-        }
 
-        secondsDisplay.textContent = seconds -1
-        secondsCountdown()
-    }, 1000)
+function resetControls() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
 }
-//Event-driven
-//programação imperativa
-//callback
-buttonPlay.addEventListener('click', function(){
-    buttonPlay.classList.add('hide')
-    buttonPause.classList.remove('hide')
-    buttonStop.classList.remove('hide')
-    buttonSet.classList.add('hide')
 
-    secondsCountdown()
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+function countdown(){
+  timerTimeOut = setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    updateTimerDisplay(minutes, 0)
+
+    if (minutes <= 0) {
+      resetControls()
+      return
+    }
+
+
+    if( seconds <= 0 ) {
+      seconds = 3
+      --minutes
+    }
+
+    updateTimerDisplay(minutes, String(seconds - 1))
+
+    countdown()
+  }, 1000)
+}
+
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
+
+  countdown()
+
 })
 
-buttonPause.addEventListener('click', function(){
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
+buttonPause.addEventListener('click', function() {
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+  clearTimeout(timerTimeOut)
 })
 
-buttonStop.addEventListener('click', function(){
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonStop.classList.add('hide')
-    buttonSet.classList.remove('hide')
+buttonStop.addEventListener('click', function() {
+  resetControls()
+  resetTimer()
 })
 
-buttonSoundON.addEventListener('click', function(){
-    buttonSoundON.classList.add('hide')
-    buttonSoundMute.classList.remove('hide')
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
 })
 
-buttonSoundMute.addEventListener('click', function(){
-    buttonSoundON.classList.remove('hide')
-    buttonSoundMute.classList.add('hide')
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
 })
 
-buttonSet.addEventListener('click', function(){
-    minutes = prompt('Quantos minutos?')
-    minutesDisplay.textContent = minutes
+buttonSet.addEventListener('click', function() {
+  let newMinutes = prompt('Quantos minutos?')
+  if (!newMinutes) {
+    resetTimer()
+    return
+  }
+
+  minutes = newMinutes
+  updateTimerDisplay(minutes, 0)
 })
