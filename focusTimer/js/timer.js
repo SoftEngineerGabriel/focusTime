32f315111c4334be0f1ccc4f057ed9e32e2239cb
SoftEngineerGabriel <123767611+SoftEngineerGabriel@users.noreply.github.com>
2023-05-01 19:33:51 -0300

    Add files via upload

--- focusTimer/js/timer.js
+++ focusTimer/js/timer.js
@@ -1,3 +1,5 @@
+import Sounds from "./sounds"
+
 export default function Timer({ 
   minutesDisplay, 
   secondsDisplay, 
@@ -31,6 +33,7 @@ export default function Timer({
       if (isFinished) {
         resetControls()
         updateDisplay()
+        Sounds().timerEnd()
         return
       }
 
